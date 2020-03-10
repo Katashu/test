eef31cd724d52fc18881f89f462e973ced2c97b8
Katashu <whomeshion@gmail.com>
2020-03-10 17:22:49 +0900

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,6 +1,12 @@
 import datetime
+import json
 
-today = datetime.datetime.today()
 
-print('hello world')
-print('Today: {}'.format(today.strftime('%Y/%m/%d')))
+def lambda_handler(event, context):
+    today = datetime.datetime.today()
+    print('hello world')
+    print('Today: {}'.format(today.strftime('%Y/%m/%d')))
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
